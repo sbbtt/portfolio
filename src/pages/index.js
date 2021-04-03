16e1fc8ef1b947a7f85d7276eeb9b196d49d4cfb
sbbtt <sbbt89@naver.com>
2021-04-03 10:55:57 +0900

    edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || '이석주의 포트폴리오입니다.'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'SeoukJu Lee'} />
       </Helmet>
       <App />
     </>
